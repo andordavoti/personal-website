f7dcdc8102d0e3b8d72a5ee5ae4923eb0ddbd222
Andor Davoti <andor.davoti@gmail.com>
2020-05-02 15:31:08 +0200

    fixed styling for desktop header dropdown

--- src/components/Header/HeaderDesktop/NavItemText/NavItemText.component.tsx
+++ src/components/Header/HeaderDesktop/NavItemText/NavItemText.component.tsx
@@ -19,7 +19,7 @@ const NavItemText: React.FC<Props> = ({ children, icon, isDropdown, dropdownHidd
     return <li className="nav-item-text" >
         <div className="text-button" onMouseEnter={() => isDropdown && hideDropdown(false)} onClick={() => isDropdown && hideDropdown(!dropdownHidden)}>
             <span className='label'>{label}</span>
-            <a className='icon'>{icon}</a>
+            <a>{icon}</a>
         </div>
 
         {!dropdownHidden && children}
