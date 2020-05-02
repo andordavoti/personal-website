cb1ca9141e74e89bd32ce59525e3c642cb31b1d1
Andor Davoti <andor.davoti@gmail.com>
2020-05-02 14:50:22 +0200

    refactoring conditional in header component

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -3,10 +3,7 @@ import HeaderDesktop from './HeaderDesktop/HeaderDesktop.component'
 import HeaderMobile from './HeaderMobile/HeaderMobile.component'
 import useMediaQuery from '../../lib/useMediaQuery'
 
-const Header: React.FC = () => {
-    const isDesktop = useMediaQuery('only screen and (min-width: 768px)')
-
-    if (isDesktop) return <HeaderDesktop />
-    return <HeaderMobile />
-}
+const Header: React.FC = () => (
+    useMediaQuery('only screen and (min-width: 768px)') ? <HeaderDesktop /> : <HeaderMobile />
+)
 export default Header
