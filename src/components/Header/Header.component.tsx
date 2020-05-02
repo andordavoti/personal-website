2c447eb8fa8825b04f04a3052de4c50a515ddd8b
Andor Davoti <andor.davoti@gmail.com>
2020-05-02 14:48:36 +0200

    added custom useMediaQuery hook to toggle between the two header components

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import HeaderDesktop from './HeaderDesktop/HeaderDesktop.component'
 import HeaderMobile from './HeaderMobile/HeaderMobile.component'
+import useMediaQuery from '../../lib/useMediaQuery'
 
-const Header: React.FC = () => (
-    <HeaderDesktop />
-)
+const Header: React.FC = () => {
+    const isDesktop = useMediaQuery('only screen and (min-width: 768px)')
 
+    if (isDesktop) return <HeaderDesktop />
+    return <HeaderMobile />
+}
 export default Header
