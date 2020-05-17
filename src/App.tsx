8a7042bfb52eeb9ac5dc826def765ffc8e2310e2
Andor Davoti <andor.davoti@gmail.com>
2020-05-17 18:47:56 +0200

    added logic for switching themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './index.css'
+import React from 'react';
+import './index.css';
 
-import Header from './components/Header/Header.component'
+import Header from './components/Header/Header.component';
+import TopIntro from './components/TopIntro/top-intro.component';
+import useMediaQuery from './lib/useMediaQuery';
+import { useDispatch } from 'react-redux';
+import { setTheme } from './redux/dropdown/settings.action';
 
-const App: React.FC = () => (
-  <Header />
-)
+const App: React.FC = () => {
+    const dispatch = useDispatch();
 
-export default App
+    const darkModeSet = useMediaQuery('(prefers-color-scheme: dark)');
+
+    if (darkModeSet) dispatch(setTheme('dark'));
+    else dispatch(setTheme('light'));
+
+    return (
+        <>
+            <Header />
+            <TopIntro />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/TopIntro/top-intro.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TopIntro: React.FC = () => {
+    return <div>Top Intro</div>;
+};
+
+export default TopIntro;
