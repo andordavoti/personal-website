b6bdf8ba993be1fcca38fd241a02b22ce3eb90fc
Andor Davoti <andor.davoti@gmail.com>
2020-05-30 13:40:25 +0200

    started using styled components and implemented the use of the breakpoints

--- src/components/TopIntro/top-intro.component.tsx
+++ src/components/TopIntro/top-intro.component.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import './top-intro.styles.scss';
+import { IntroWrapper, IntroTextWrapper, ProfileImg, IntroTitle, IntroDesc } from './top-intro.styles';
 
 const TopIntro: React.FC = () => {
     return (
-        <div className="top-intro">
-            <div className="intro-text">
-                <h2 className="header-text">
-                    I am a software developer from Oslo, Norway.
-                </h2>
-                <p className="description">
-                    With a passion for DIY I've build a lot of software and
-                    hardware projects over the years, which you can check out
-                    bellow.
-                </p>
-            </div>
-            <img
-                className="profile-img"
-                src={require('../../assets/img/profile_img.png')}
-                alt="profile"
-            />
-        </div>
+        <IntroWrapper>
+            <IntroTextWrapper>
+                <IntroTitle>I am a software developer from Oslo, Norway.</IntroTitle>
+                <IntroDesc>
+                    With a passion for DIY I've build a lot of software and hardware projects over the years, which you
+                    can check out bellow.
+                </IntroDesc>
+            </IntroTextWrapper>
+            <ProfileImg className="profile-img" src={require('../../assets/img/profile_img.png')} alt="profile" />
+        </IntroWrapper>
     );
 };
 
---
+++ src/components/TopIntro/top-intro.styles.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { breakpoints } from '../../lib/breakpoints';
+
+export const IntroWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 10px;
+
+    @media ${breakpoints.s} {
+        margin: 40px;
+    }
+`;
+
+export const IntroTextWrapper = styled.div`
+    width: 75%;
+    margin: 10px;
+
+    @media ${breakpoints.s} {
+        width: 50%;
+    }
+`;
+
+export const IntroTitle = styled.h2`
+    color: white;
+`;
+
+export const IntroDesc = styled.p`
+    color: white;
+`;
+
+export const ProfileImg = styled.img`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+`;
