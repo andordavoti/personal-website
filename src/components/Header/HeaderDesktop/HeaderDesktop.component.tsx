803518ce2754fd74ed835ada303e6b6ef5b02cb1
Andor Davoti <andor.davoti@gmail.com>
2020-06-14 23:08:42 +0200

    ditched my NavItemIcon component for IconButton in MUI

--- src/components/Header/HeaderDesktop/HeaderDesktop.component.tsx
+++ src/components/Header/HeaderDesktop/HeaderDesktop.component.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './HeaderDesktop.styles.scss';
 
 // Components
 import Navbar from '../Navbar/Navbar.component';
-import NavItemIcon from './NavItemIcon/NavItemIcon.component';
 
 // Icons
 import HomeIcon from '@material-ui/icons/Home';
@@ -11,23 +11,67 @@ import BuildIcon from '@material-ui/icons/Build';
 import EmailIcon from '@material-ui/icons/Email';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import IconButton from '@material-ui/core/IconButton';
+import socialLinks from '../../../lib/socialLinks';
+import { SocialLinkType } from '../../../lib/types';
+import { makeStyles } from '@material-ui/core';
 
-const HeaderDesktop: React.FC = () => (
-    <div className="header">
-        <Navbar>
-            <div className="title-container">
-                <h1 className="title">Andor Davoti</h1>
-            </div>
+const useStyles = makeStyles((theme) => ({
+    center: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+}));
 
-            <NavItemIcon icon={<HomeIcon />} toPath="/" />
+const HeaderDesktop: React.FC = () => {
+    const styles = useStyles();
+    return (
+        <div className="header">
+            <Navbar>
+                <div className="title-container">
+                    <h1 className="title">Andor Davoti</h1>
+                </div>
 
-            <NavItemIcon icon={<BuildIcon />} toPath="/projects" />
+                <Link to="/" className={styles.center}>
+                    <IconButton>
+                        <HomeIcon />
+                    </IconButton>
+                </Link>
 
-            <NavItemIcon link="email" icon={<EmailIcon />} />
-            <NavItemIcon link="linkedin" icon={<LinkedInIcon />} />
-            <NavItemIcon link="github" icon={<GitHubIcon />} />
-        </Navbar>
-    </div>
-);
+                <Link to="/projects" className={styles.center}>
+                    <IconButton>
+                        <BuildIcon />
+                    </IconButton>
+                </Link>
+
+                <div className={styles.center}>
+                    <IconButton rel="noopener noreferrer" target="_blank" href={socialLinks['email' as SocialLinkType]}>
+                        <EmailIcon />
+                    </IconButton>
+                </div>
+
+                <div className={styles.center}>
+                    <IconButton
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href={socialLinks['linkedin' as SocialLinkType]}
+                    >
+                        <LinkedInIcon />
+                    </IconButton>
+                </div>
+
+                <div className={styles.center}>
+                    <IconButton
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href={socialLinks['github' as SocialLinkType]}
+                    >
+                        <GitHubIcon />
+                    </IconButton>
+                </div>
+            </Navbar>
+        </div>
+    );
+};
 
 export default HeaderDesktop;
