11fb36483357ccd9a6f2a4e8d9dc20ccbc0acf55
Andor Davoti <andor.davoti@gmail.com>
2020-06-14 23:49:13 +0200

    renamed header component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { useDispatch } from 'react-redux';
 import { setTheme } from './redux/dropdown/settings.action';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './lib/theme';
-import Header from './components/Header/Header.component';
+import Header from './components/Header/Header';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 
