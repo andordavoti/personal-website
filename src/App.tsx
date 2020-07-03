7c092f1cf0641152caff37e278eae627e9ccd1ab
Andor Davoti <andor.davoti@gmail.com>
2020-07-03 20:39:53 +0200

    light/dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './index.css';
 
 import { Switch, Route } from 'react-router-dom';
@@ -6,13 +6,13 @@ import { Switch, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setTheme } from './redux/dropdown/settings.action';
 import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './lib/theme';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
-import { useMediaQuery } from '@material-ui/core';
+import { useMediaQuery, createMuiTheme } from '@material-ui/core';
 import ProjectDetailsPage from './pages/ProjectDetailsPage';
 import ErrorBoundary from './components/ErrorBoundary';
+import { paletteDark, paletteLight } from './lib/theme';
 
 const App: React.FC = () => {
     const dispatch = useDispatch();
@@ -21,15 +21,18 @@ const App: React.FC = () => {
     if (darkModeSet) dispatch(setTheme('dark'));
     else dispatch(setTheme('light'));
 
+    const theme = useMemo(() => createMuiTheme({ palette: darkModeSet ? paletteDark : paletteLight }), [darkModeSet]);
     return (
         <ThemeProvider theme={theme}>
-            <Header />
-            <ErrorBoundary>
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route path="/:projectId" component={ProjectDetailsPage} />
-                </Switch>
-            </ErrorBoundary>
+            <div className="content-wrapper">
+                <Header />
+                <ErrorBoundary>
+                    <Switch>
+                        <Route exact path="/" component={HomePage} />
+                        <Route path="/:projectId" component={ProjectDetailsPage} />
+                    </Switch>
+                </ErrorBoundary>
+            </div>
             <Footer />
         </ThemeProvider>
     );
--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,23 +1,38 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { PaletteOptions } from '@material-ui/core/styles/createPalette';
 
-const theme = createMuiTheme({
-    palette: {
-        type: 'dark',
-        primary: {
-            main: '#ffffff',
-        },
-        secondary: {
-            main: '#b5b5b5',
-        },
-        text: {
-            primary: '#ffffff',
-            secondary: '#b5b5b5',
-        },
-        background: {
-            default: '#151616',
-            paper: '#242526',
-        },
+export const paletteDark: PaletteOptions = {
+    type: 'dark',
+    primary: {
+        main: '#ffffff',
     },
-});
+    secondary: {
+        main: '#b5b5b5',
+    },
+    text: {
+        primary: '#ffffff',
+        secondary: '#b5b5b5',
+    },
+    background: {
+        default: '#151616',
+        paper: '#242526',
+    },
+};
 
-export default theme;
+// change these values
+export const paletteLight: PaletteOptions = {
+    type: 'light',
+    primary: {
+        main: '#ffffff',
+    },
+    secondary: {
+        main: '#b5b5b5',
+    },
+    text: {
+        primary: '#ffffff',
+        secondary: '#b5b5b5',
+    },
+    background: {
+        default: '#151616',
+        paper: '#242526',
+    },
+};
