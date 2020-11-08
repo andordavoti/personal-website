a9e376b5ed2cb7ffa9637abe8d85806638122c5e
Andor Davoti <andor.davoti@gmail.com>
2020-11-08 14:06:49 +0100

    fixed image paths

--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -5,59 +5,59 @@ const skills: SkillsType = {
         {
             name: 'React',
             link: 'https://reactjs.org/',
-            imgUrl: require('../assets/icons/skill-icons/react.png'),
+            imgUrl: require('../../assets/icons/skill-icons/react.png'),
         },
         {
             name: 'React Native',
             link: 'https://reactnative.dev/',
-            imgUrl: require('../assets/icons/skill-icons/react.png'),
+            imgUrl: require('../../assets/icons/skill-icons/react.png'),
         },
         {
             name: 'Javascript',
             link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-            imgUrl: require('../assets/icons/skill-icons/JS.jpg'),
+            imgUrl: require('../../assets/icons/skill-icons/JS.jpg'),
         },
         {
             name: 'Typescript',
             link: 'https://www.typescriptlang.org/',
-            imgUrl: require('../assets/icons/skill-icons/TS.jpg'),
+            imgUrl: require('../../assets/icons/skill-icons/TS.jpg'),
         },
         {
             name: 'HTML',
             link: 'https://www.w3.org/html/',
-            imgUrl: require('../assets/icons/skill-icons/HTML.png'),
+            imgUrl: require('../../assets/icons/skill-icons/HTML.png'),
         },
         {
             name: 'CSS & SCSS',
             link: 'https://sass-lang.com/',
-            imgUrl: require('../assets/icons/skill-icons/CSS.png'),
+            imgUrl: require('../../assets/icons/skill-icons/CSS.png'),
         },
         {
             name: 'Arduino',
             link: 'https://www.arduino.cc/',
-            imgUrl: require('../assets/icons/skill-icons/arduino.png'),
+            imgUrl: require('../../assets/icons/skill-icons/arduino.png'),
         },
         {
             name: 'Firebase',
             link: 'https://firebase.google.com/',
-            imgUrl: require('../assets/icons/skill-icons/firebase.png'),
+            imgUrl: require('../../assets/icons/skill-icons/firebase.png'),
         },
     ],
     design: [
         {
             name: 'Adobe XD',
             link: 'https://www.adobe.com/products/xd.html',
-            imgUrl: require('../assets/icons/skill-icons/XD.png'),
+            imgUrl: require('../../assets/icons/skill-icons/XD.png'),
         },
         {
             name: 'Adobe Photoshop',
             link: 'https://www.adobe.com/products/photoshop.html',
-            imgUrl: require('../assets/icons/skill-icons/PS.png'),
+            imgUrl: require('../../assets/icons/skill-icons/PS.png'),
         },
         {
             name: 'Fusion 360',
             link: 'https://www.autodesk.com/products/fusion-360/overview',
-            imgUrl: require('../assets/icons/skill-icons/fusion-360.jpg'),
+            imgUrl: require('../../assets/icons/skill-icons/fusion-360.jpg'),
         },
     ],
 };
